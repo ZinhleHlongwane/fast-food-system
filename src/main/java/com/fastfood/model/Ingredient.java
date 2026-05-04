e8990cf98266ef88a5754285f0ba1c55794f3c3d
Zinhle Hlongwane <zihlojhb025@student.wethinkcode.co.za>
2026-05-04 15:08:26 +0200

    Complete ingredient.java file

--- src/main/java/com/fastfood/model/Ingredient.java
+++ src/main/java/com/fastfood/model/Ingredient.java
@@ -6,9 +6,32 @@ public class Ingredient {
     private int quantity;
 
     public Ingredient(String name, int quantity) {
+        validateQuantity(quantity);
         this.name = name;
         this.quantity = quantity;
     }
 
-    
+    public String name() {
+        return this.name;
+    }
+
+    public int quantity() {
+        return this.quantity;
+    }
+
+    public void updateQuantity(int quantity) {
+        validateQuantity(quantity);
+        this.quantity = quantity;
+    }
+
+    private void validateQuantity(int quantity) {
+        if (quantity < 0) {
+            throw new IllegalArgumentException("Quantity cannot be less than 0");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name() + ", Quantity: " + quantity();
+    }
 }
